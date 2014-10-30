72a353ea634bea2ca90a16c9c27a63a04bc09dfa
Candice Newton <candice.c.newton@gmail.com>
2014-10-30 12:36:55 -0700

    Adding content-top region, layout adjustments

--- public/sites/all/themes/bitch/templates/page.tpl.php
+++ public/sites/all/themes/bitch/templates/page.tpl.php
@@ -41,22 +41,36 @@
   <div id="main">
 
     <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+
+      <?php
+        // Render the content top to see if there's anything in it
+        $content_top  = render($page['content_top']);
+      ?>
+
+      <?php if ($content_top): ?>
+        <div id="content-top">
+          <?php print $content_top; ?>
+        </div>
       <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
+
+      <div class="content-inner">
+        <?php print render($page['highlighted']); ?>
+        <?php print $breadcrumb; ?>
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php print $messages; ?>
+        <?php print render($tabs); ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <?php print render($page['content']); ?>
+        <?php print $feed_icons; ?>
+      </div>
     </div>
 
     <?php
